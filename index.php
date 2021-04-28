f7d6f91266a7dafcb4a62f2ad990b9da80144eea
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-04-28 16:11:31 +0200

    Esercizio completato

--- index.php
+++ index.php
@@ -7,16 +7,19 @@
   <title>Todos</title>
 </head>
 <body>
-  <div class="">
-    <?php $paragrafo = "Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+  <div class="paragrafo">
+    <?php $paragrafo = "Ipsum lorem ipsum dolor sit amet, consectetur adipisicing lorem,
     sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-    minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-    non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." ?>
-    <?php echo "$paragrafo"; ?>
+    minim veniam, quis nostrud exercitation lorem laboris nisi ut aliquip ex ea
+    lorem consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat lorem
+    non proident, lorem in culpa qui officia deserunt mollit anim id est laborum." ?>
   </div>
-  <div class="">
+  <div class="search-world">
+    <?php $bad = $_GET["word"]; ?>
+    <?php echo str_replace($bad, '***', $paragrafo) ?>
+  </div>
+  <div class="length">
     <span>Il paragrafo è lungo <?php echo strlen($paragrafo) ?> caratteri.</span>
   </div>
 </body>
